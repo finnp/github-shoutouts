99057dc9aee4f760cdb2b21fad4c4ec59dc1b802
finnp <derfinn@gmail.com>
2016-04-18 15:38:02 +0200

    ranem index.js to fetch.js, add github lib

--- pr-counts.js
+++ pr-counts.js
@@ -1,30 +1,28 @@
-var levelup = require('level')
 var afterAll = require('after-all')
 var github = require('./lib/github')
 
-var db = levelup('./db', {valueEncoding: 'json'})
+module.exports = function (db, cb) {
+  var users = {}
 
-var users = {}
-
-var next = afterAll(function (err) {
-  if (err) return console.error(err)
-  console.log(users)
-  db.put('pr_count', users, function () {
-    console.log('done')
+  var next = afterAll(function (err) {
+    if (err) return console.error(err)
+    db.put('pr_count', users, function () {
+      cb()
+    })
   })
-})
 
-github.createStream('/orgs/hoodiehq/repos')
+  github.createStream('/orgs/hoodiehq/repos')
   .on('data', function (repo) {
     github.createStream(repo.url + '/pulls?state=closed')
-      .on('data', function (pull) {
-        if (!pull.merged_at) return
+    .on('data', function (pull) {
+      if (!pull.merged_at) return
 
-        var user = pull.user.login
-        console.log(user)
-        if (users[user]) users[user]++
-        else users[user] = 1
-      })
-      .on('end', next())
+      var user = pull.user.login
+      console.log(user)
+      if (users[user]) users[user]++
+      else users[user] = 1
+    })
+    .on('end', next())
   })
   .on('end', next())
+}
